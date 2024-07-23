0c33682d6330d5d59eb365ebb728783685d93b60
Ricky - <rickyjay567@gmail.com>
2024-07-23 20:52:07 +0700

    fix: changing domain

--- lib/mail.ts
+++ lib/mail.ts
@@ -6,7 +6,7 @@ const domain = process.env.NEXT_PUBLIC_APP_URL;
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
   await resend.emails.send({
-    from: "onboarding@ric-portfolio.dev",
+    from: "onboarding@ric-portfolio.web.id",
     to: email,
     subject: "Confirm your email",
     html: `<p>Click <a href="${confirmLink}">here</a></p>`,
@@ -16,7 +16,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 export const sendPasswordEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-password?token=${token}`;
   await resend.emails.send({
-    from: "onboarding@ric-portfolio.dev",
+    from: "onboarding@ric-portfolio.web.id",
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${confirmLink}">here</a></p>`,
@@ -25,7 +25,7 @@ export const sendPasswordEmail = async (email: string, token: string) => {
 
 export const sendTwoFactorEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: "onboarding@ric-portfolio.dev",
+    from: "onboarding@ric-portfolio.web.id",
     to: email,
     subject: "2FA Code",
     html: `<p>This is your 2FA code ${token}</p>`,
